0de89befe4466a74404cc14595eefee8a19261b1
wmarcela <wmarcela@student.agh.edu.pl>
2024-03-23 01:34:35 +0100

    feat: "Parser finished, exceptions caught'

---
+++ json_parser.py
@@ -0,0 +1,39 @@
+import json
+
+
+def check_json_resource(file_path: str):
+    # Try block to check if the JSON file exists
+    try:
+        with open(file_path, "r") as json_file:
+            data = json.load(json_file)
+
+    except json.decoder.JSONDecodeError:
+        return "File structure is not valid JSON."
+    except (FileNotFoundError, TypeError, ValueError, OSError):
+        return "File not found, try again."
+
+    # Try block to check if the file structure is valid AWS Policy JSON 
+    try:
+        policy_body = data["PolicyDocument"]
+
+        # Check if key is present
+        policy_body["Version"]
+
+        # It is possible that policy file contains multiple statements
+        statements = policy_body["Statement"]
+        # Because of multiple resources (one per statement), we provide a list to store multiple logical values
+        bool_list = []
+        for statement in statements:
+            # Check if keys are present
+            statement["Effect"]
+            statement["Action"]
+
+            if statement["Resource"] != "*":
+                bool_list.append(True)
+            else:
+                bool_list.append(False)
+
+    except KeyError:
+        return "File has invalid AWS Policy document structure."
+
+    return bool_list
--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-import json
+from json_parser import check_json_resource
 
-
-def check_json_resource(file_path):
-    with open(file_path, "r") as json_file:
-        data = json.load(json_file)
-
-        # It is possible that JSON file contains multiple statements
-        statements = len(data["PolicyDocument"]["Statement"])
-        print(statements)
-
-        # Because of multiple resources we need multiple logical statements stored in list
-        bool_list = []
-
-        for i in range(statements):
-            if data["PolicyDocument"]["Statement"][i]["Resource"] != "*":
-                print(data["PolicyDocument"]["Statement"][i]["Resource"])
-                bool_list.append(True)
-            else:
-                print(data["PolicyDocument"]["Statement"][i]["Resource"])
-                bool_list.append(False)
-
-        return bool_list
-
-
-print(check_json_resource("JSON files/test6.json"))
+policy_file_path = "JSON files/test6.json"
+print(check_json_resource(policy_file_path))
