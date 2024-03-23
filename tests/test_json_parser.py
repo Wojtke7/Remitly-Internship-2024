c8ff0f4fe628070a06812087e97851fbd1f0e2e7
wmarcela <wmarcela@student.agh.edu.pl>
2024-03-23 21:36:18 +0100

    fix: tests refactored

--- tests/test_json_parser.py
+++ tests/test_json_parser.py
@@ -1,50 +1,40 @@
 import unittest
-from json_parser import check_json_resource
+import json
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from json_parser import json_parser
 
 
 class TestJsonParser(unittest.TestCase):
     def setUp(self):
-        self.invalid_path = "invalid_paXth.json"
-        self.invalid_json_struct = "test_jsons/invalid_syntax.json"
-        self.invalid_AWS_struct = "test_jsons/invalid_AWS_struct.json"
-        self.valid_AWS_json = "test_jsons/valid.json"
+        self.invalid_path = "invalidpath123.json"
+        self.invalid_json_struct = "tests/data/invalid_syntax.json"
+        self.invalid_AWS_struct = "tests/data/invalid_aws_struct.json"
+        self.valid_AWS_json = "tests/data/valid.json"
 
     def test_file_not_found(self):
-        self.assertEqual(
-            check_json_resource(self.invalid_path),
-            "File not found, try again."
-        )
+        with self.assertRaises(FileNotFoundError):
+            json_parser(self.invalid_path)
 
     def test_invalid_argument(self):
         invalid_args = [15, ["list"], True, (1, 3, 4, 6), {"key": "value"}]
-        expected_error_msg = "File not found, try again."
 
         for arg in invalid_args:
-            self.assertEqual(
-                check_json_resource(arg),
-                expected_error_msg
-            )
+            with self.assertRaises(Exception):
+                json_parser(arg)
 
     def test_invalid_json(self):
-        self.assertEqual(
-            check_json_resource(self.invalid_json_struct),
-            "File structure is not valid JSON."
-        )
+        with self.assertRaises(json.decoder.JSONDecodeError):
+            json_parser(self.invalid_json_struct)
 
     def test_invalid_policy_document(self):
-        self.assertEqual(
-            check_json_resource(self.invalid_AWS_struct),
-            "File has invalid AWS Policy document structure."
-        )
+        with self.assertRaises(KeyError):
+            json_parser(self.invalid_AWS_struct)
 
     def test_correct_return(self):
-        self.assertIsInstance(
-            check_json_resource(self.valid_AWS_json),
-            list)
-        self.assertEqual(
-            check_json_resource(self.valid_AWS_json),
-            [False, False, True]
-        )
-        
+        result = json_parser(self.valid_AWS_json)
+        self.assertIsInstance(result, list)
+        self.assertEqual(result, [False, False, True])
+
 if __name__ == '__main__':
     unittest.main()
